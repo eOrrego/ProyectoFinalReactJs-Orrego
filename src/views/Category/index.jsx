f5ee55fd9c9e718598236a702814cf092353a73b
SteveO <orregoe@gmail.com>
2023-12-10 23:30:25 -0300

    Refactor Category page to use asyncMock data

--- src/views/Category/index.jsx
+++ src/views/Category/index.jsx
@@ -1,38 +1,50 @@
-import data from '../../data/products.json';
+import { getProductsByCategory } from "../../data/asyncMock";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-
-const { data: { items } } = data;
+import CardProduct from "../../components/CardProduct/CardProduct";
 
 const PageProductCategory = () => {
+    const [products, setProducts] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(false);
 
     const { categoryId } = useParams();
 
-    const products = items.filter((item) => item.category === categoryId);
+    useEffect(() => {
+        const asyncFuction = async () => {
+            try {
+                const result = await getProductsByCategory(categoryId);
+                setProducts(result);
+            } catch (error) {
+                setError(true);
+            } finally {
+                setLoading(false);
+            }
+        };
+        asyncFuction();
+    }, [categoryId]);
+
+    if (loading) {
+        return (
+            <div className="container">
+                <h2 className='text-center text-uppercase my-5'>Cargando...</h2>
+            </div>
+        );
+    }
+
+    if (error) {
+        return (
+            <div className="container">
+                <h2 className='text-center text-uppercase my-5'>Hubo un error</h2>
+            </div>
+        );
+    }
 
     return (
-        <>
-            <div className="container">
-                <h2>
-                    {categoryId.toUpperCase()}
-                </h2>
-                <div className="row">
-                    {products.map((product) => {
-                        return (
-                            <div className="col-md-4" key={`product-${product.id}`} >
-                                <div className="card">
-                                    <h6>{product.category}</h6>
-                                    <img src={product.images} className="card-img-top" alt={product.name} />
-                                    <div className="card-body">
-                                        <h5 className="card-title">{product.name}</h5>
-                                        <p className="card-text">${product.price}</p>
-                                    </div>
-                                </div>
-                            </div>
-                        );
-                    })}
-                </div>
-            </div>
-        </>
+        <div className="container">
+            <h2 className="text-center text-uppercase my-5">{categoryId}</h2>
+            <CardProduct products={products} />
+        </div>
     )
 }
 
