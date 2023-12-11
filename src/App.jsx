aa960bd946f8713c7fa989310cc369b5a5dbd80b
SteveO <orregoe@gmail.com>
2023-12-10 22:58:16 -0300

    Remove commented out code

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,6 @@
 import Routes from "./routes";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
-// import ItemListContainer from "./components/ItemListContainer/ItemListContainer";
-// import NavbarPage from "./components/Navbar/NavbarPage";
 
 const App = () => <Routes />;
 
