316702a79c00538bca151989e0c56acaaa546573
SteveO <orregoe@gmail.com>
2024-01-06 00:04:05 -0300

    Refactor code to use Firebase instead of asyncMock

--- src/views/Category/index.jsx
+++ src/views/Category/index.jsx
@@ -1,27 +1,43 @@
-import { getProductsByCategory } from "../../data/asyncMock";
+// import { getProductsByCategory } from "../../data/asyncMock";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import CardProduct from "../../components/CardProduct/CardProduct";
 
+//reemplazar asyncMock por firebase
+import { getDocs, collection, query, where } from 'firebase/firestore';
+import { db } from '../../services/firebase/config.js';
+
 const PageProductCategory = () => {
     const [products, setProducts] = useState([]);
     const [loading, setLoading] = useState(true);
-    const [error, setError] = useState(false);
+    // const [error, setError] = useState(false);
 
     const { categoryId } = useParams();
 
+    // useEffect(() => {
+    //     const asyncFuction = async () => {
+    //         try {
+    //             const result = await getProductsByCategory(categoryId);
+    //             setProducts(result);
+    //         } catch (error) {
+    //             setError(true);
+    //         } finally {
+    //             setLoading(false);
+    //         }
+    //     };
+    //     asyncFuction();
+    // }, [categoryId]);
+
     useEffect(() => {
-        const asyncFuction = async () => {
-            try {
-                const result = await getProductsByCategory(categoryId);
-                setProducts(result);
-            } catch (error) {
-                setError(true);
-            } finally {
-                setLoading(false);
-            }
+        const getProductsFirebase = async () => {
+            const productsCollection = collection(db, 'products');
+            const q = query(productsCollection, where('category', '==', categoryId));
+            const productsSnapshot = await getDocs(q);
+            const productsList = productsSnapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
+            setProducts(productsList);
+            setLoading(false);
         };
-        asyncFuction();
+        getProductsFirebase();
     }, [categoryId]);
 
     if (loading) {
@@ -32,13 +48,13 @@ const PageProductCategory = () => {
         );
     }
 
-    if (error) {
-        return (
-            <div className="container">
-                <h2 className='text-center text-uppercase my-5'>Hubo un error</h2>
-            </div>
-        );
-    }
+    // if (error) {
+    //     return (
+    //         <div className="container">
+    //             <h2 className='text-center text-uppercase my-5'>Hubo un error</h2>
+    //         </div>
+    //     );
+    // }
 
     return (
         <div className="container">
