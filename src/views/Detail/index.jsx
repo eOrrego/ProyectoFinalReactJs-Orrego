68e42559c4b368d010372046d78be201acaf33ae
SteveO <orregoe@gmail.com>
2024-01-06 00:10:32 -0300

    Refactor getProductById to use Firebase for fetching product by id

--- src/views/Detail/index.jsx
+++ src/views/Detail/index.jsx
@@ -1,27 +1,42 @@
-import { getProductById } from "../../data/asyncMock";
+// import { getProductById } from "../../data/asyncMock";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import ItemDetailContainer from "../../components/ItemDetailContainer";
 
+//reemplazar asyncMock por firebase para obtener un producto por id
+import { doc, getDoc } from 'firebase/firestore';
+import { db } from '../../services/firebase/config.js';
+
 const PageProductDetail = () => {
     const [product, setProduct] = useState([]);
     const [loading, setLoading] = useState(true);
-    const [error, setError] = useState(false);
+    // const [error, setError] = useState(false);
 
     const { productId } = useParams();
 
+    // useEffect(() => {
+    //     const asyncFuction = async () => {
+    //         try {
+    //             const result = await getProductById(productId);
+    //             setProduct(result);
+    //         } catch (error) {
+    //             setError(true);
+    //         } finally {
+    //             setLoading(false);
+    //         }
+    //     };
+    //     asyncFuction();
+    // }, [productId]);
+
     useEffect(() => {
-        const asyncFuction = async () => {
-            try {
-                const result = await getProductById(productId);
-                setProduct(result);
-            } catch (error) {
-                setError(true);
-            } finally {
-                setLoading(false);
-            }
+        const getProductFirebase = async () => {
+            const productDoc = doc(db, 'products', productId);
+            const productSnapshot = await getDoc(productDoc);
+            const productData = productSnapshot.data();
+            setProduct({ id: productSnapshot.id, ...productData });
+            setLoading(false);
         };
-        asyncFuction();
+        getProductFirebase();
     }, [productId]);
 
     if (loading) {
@@ -32,13 +47,13 @@ const PageProductDetail = () => {
         );
     }
 
-    if (error) {
-        return (
-            <div className="container">
-                <h2 className='text-center text-uppercase my-5'>Hubo un error</h2>
-            </div>
-        );
-    }
+    // if (error) {
+    //     return (
+    //         <div className="container">
+    //             <h2 className='text-center text-uppercase my-5'>Hubo un error</h2>
+    //         </div>
+    //     );
+    // }
 
     return (
         <>
