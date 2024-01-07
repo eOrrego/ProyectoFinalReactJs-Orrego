6e2611c75b04fe2651397ea39dbaebe40c67ee9e
SteveO <orregoe@gmail.com>
2024-01-07 00:59:52 -0300

    Add AuthProvider to App component

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,15 @@ import Routes from "./routes";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import { CartProvider } from "./context/CartContext";
+import { AuthProvider } from "./context/FirebaseAuthContext";
 
 const App = () => {
     return (
-        <CartProvider>
-            <Routes />
-        </CartProvider>
+        <AuthProvider>
+            <CartProvider>
+                <Routes />
+            </CartProvider>
+        </AuthProvider>
     );
 }
 
