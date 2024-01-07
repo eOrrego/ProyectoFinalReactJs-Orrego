508db7d25d31a6f38d417f5ba7efd27b7b230351
SteveO <orregoe@gmail.com>
2024-01-07 01:05:49 -0300

    Add login functionality to LoginPage component

--- src/views/LoginPage/index.jsx
+++ src/views/LoginPage/index.jsx
@@ -1,6 +1,39 @@
+import { useState } from 'react'
+import { useAuth } from '../../context/FirebaseAuthContext'
+
 const LoginPage = () => {
+    const { login, loginWithGoogle } = useAuth();
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const handleLogin = (e) => {
+        e.preventDefault();
+        login(email, password);
+    }
+
+    const handleGoogleLogin = (e) => {
+        e.preventDefault();
+        loginWithGoogle();
+    }
+
+    const handleEmailChange = (e) => {
+        setEmail(e.target.value);
+    }
+
+    const handlePasswordChange = (e) => {
+        setPassword(e.target.value)
+    }
+
     return (
-        <div>LoginPage</div>
+        <>
+            <h1>Login</h1>
+            <form onSubmit={handleLogin}>
+                <input type="email" placeholder="Email" onChange={handleEmailChange} />
+                <input type="password" placeholder="Password" onChange={handlePasswordChange} />
+                <button type="submit">Login</button>
+            </form>
+            <button onClick={handleGoogleLogin}>Login with Google</button>
+        </>
     )
 }
 
