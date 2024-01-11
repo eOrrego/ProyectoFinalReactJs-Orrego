303d3cd98c1f5ecd1e93b50ff73dc254c70ad4ec
SteveO <orregoe@gmail.com>
2024-01-10 23:26:04 -0300

    Refactor Detail component to use Firebase instead of asyncMock

--- src/views/Detail/index.jsx
+++ src/views/Detail/index.jsx
@@ -1,32 +1,14 @@
-// import { getProductById } from "../../data/asyncMock";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import ItemDetailContainer from "../../components/ItemDetailContainer";
-
-//reemplazar asyncMock por firebase para obtener un producto por id
 import { doc, getDoc } from 'firebase/firestore';
 import { db } from '../../services/firebase/config.js';
 
 const PageProductDetail = () => {
     const [product, setProduct] = useState([]);
     const [loading, setLoading] = useState(true);
-    // const [error, setError] = useState(false);
 
     const { productId } = useParams();
-
-    // useEffect(() => {
-    //     const asyncFuction = async () => {
-    //         try {
-    //             const result = await getProductById(productId);
-    //             setProduct(result);
-    //         } catch (error) {
-    //             setError(true);
-    //         } finally {
-    //             setLoading(false);
-    //         }
-    //     };
-    //     asyncFuction();
-    // }, [productId]);
 
     useEffect(() => {
         const getProductFirebase = async () => {
@@ -46,14 +28,6 @@ const PageProductDetail = () => {
             </div>
         );
     }
-
-    // if (error) {
-    //     return (
-    //         <div className="container">
-    //             <h2 className='text-center text-uppercase my-5'>Hubo un error</h2>
-    //         </div>
-    //     );
-    // }
 
     return (
         <>
