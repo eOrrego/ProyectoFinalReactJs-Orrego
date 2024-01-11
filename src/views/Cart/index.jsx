b242656ee335bb89d10c6df73f485e800a1dfe35
SteveO <orregoe@gmail.com>
2024-01-11 18:04:38 -0300

    Refactor Cart component UI

--- src/views/Cart/index.jsx
+++ src/views/Cart/index.jsx
@@ -8,12 +8,27 @@ const Cart = () => {
 
     if (getQuantity() === 0) {
         return (
-            <div className="container">
-                <div className="row">
-                    <div className="col-12 text-center">
-                        <h1 className="display-1">Carrito</h1>
-                        <h2 className="display-4">Tu carrito está vacío</h2>
-                        <Link to="/" className="btn btn-primary">Volver al inicio</Link>
+            <div className="
+            container
+            ">
+                <div className="
+                row
+                ">
+                    <div className="
+                    col-12 
+                    text-center
+                    ">
+                        <h2 className="
+                        display-3
+                        ">Carrito</h2>
+                        <h3 className="
+                        display-5
+                        mb-5
+                        ">Tu carrito está vacío</h3>
+                        <Link to="/" className="
+                        btn 
+                        btn-outline-primary
+                        ">Volver al inicio</Link>
                     </div>
                 </div>
             </div>
@@ -25,17 +40,28 @@ const Cart = () => {
         <div className="container">
             <div className="row">
                 <div className="col-12 text-center">
-                    <h1 className="display-1">Carrito</h1>
-                    <h2 className="display-4">Total: ${getTotalPrice()}</h2>
-                    <Link to="/" className="btn btn-primary">Volver al inicio</Link>
-                    <button className="btn btn-danger" onClick={clearCart}>Vaciar carrito</button>
+                    <h2 className="display-3">Carrito</h2>
+                    <h3 className="display-5">Total: ${getTotalPrice()}</h3>
+                    <Link to="/" className="btn btn-outline-primary btn-sm mx-2">Volver al inicio</Link>
+                    <button className="btn btn-outline-danger btn-sm mx-2" onClick={clearCart}>Vaciar carrito</button>
                 </div>
             </div>
             <div className="row">
-                <div className="col-12">
-                    <table className="table table-striped">
+                <div className="
+                col-11
+                my-5
+                mx-auto
+                ">
+                    <table className="
+                    table 
+                    table-striped
+                    table-hover
+                    table-bordered
+                    text-black
+                    ">
                         <thead>
                             <tr>
+                                <th scope="col"></th>
                                 <th scope="col">Producto</th>
                                 <th scope="col">Cantidad</th>
                                 <th scope="col">Precio unitario</th>
@@ -46,11 +72,18 @@ const Cart = () => {
                         <tbody>
                             {cartItems.map((item) => (
                                 <tr key={item.id}>
-                                    <td>{item.name}</td>
+                                    <td><img src={item.images} alt={item.name} width="50" /></td>
+                                    <td className="
+                                    align-middle
+                                    ">{item.name}</td>
                                     <td>{item.quantity}</td>
                                     <td>${item.price}</td>
                                     <td>${item.price * item.quantity}</td>
-                                    <td><button className="btn btn-danger" onClick={() => removeFromCart(item.id)}>X</button></td>
+                                    <td><button className="
+                                    btn 
+                                    btn-outline-danger
+                                    btn-sm
+                                    " onClick={() => removeFromCart(item.id)}>X</button></td>
                                 </tr>
                             ))}
                         </tbody>
@@ -58,7 +91,9 @@ const Cart = () => {
                 </div>
             </div>
             <hr />
-            <Link to="/checkout" className="btn btn-success">Finalizar compra</Link>
+            <Link to="/checkout" className="
+            btn btn-outline-success btn-lg
+            ">Finalizar compra</Link>
         </div>
 
     )
