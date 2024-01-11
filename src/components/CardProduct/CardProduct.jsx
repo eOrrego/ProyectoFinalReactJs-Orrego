0e134241e9101633b2c18129cea2cc25bcc31a1c
SteveO <orregoe@gmail.com>
2024-01-11 17:10:55 -0300

    Refactor CardProduct component layout and styles

--- src/components/CardProduct/CardProduct.jsx
+++ src/components/CardProduct/CardProduct.jsx
@@ -4,29 +4,72 @@ import { Link } from "react-router-dom";
 /* eslint-disable react/prop-types */
 const CardProduct = ({ products }) => {
     return (
-        <div className="row">
+        <div className="
+        row
+        g-4
+        my-5
+        ">
             {products.map((product) => {
                 return (
-                    <div className="col-md-4" key={`product-${product.id}`} >
-                        <div className="card">
-                            <h6>{product.category}</h6>
+                    <div className="
+                    col-md-4
+                    d-flex
+                    justify-content-center
+                    align-items-center
+                    " key={`product-${product.id}`} >
+                        <div
+                            className="
+                        card
+                        text-center
+                        text-black
+                        bg-light
+                        mb-3"
+                            style={{
+                                width: "20rem",
+                                height: "30rem",
+                            }}
+                        >
+                            <h6
+                                className="
+                                card-header
+                                text-uppercase
+                                font-weight-bold
+                                "
+                            >{product.category}</h6>
                             <img
                                 src={product.images}
                                 className="
                                 card-img-top
-                                img-fluid
-                                rounded-start
-                                d-block
-                                mx-auto
-                                w-50
-                                h-50
+                                border-bottom
+                                border-secondary
                                 "
                                 alt={product.name}
+                                style={{
+                                    width: "100%",
+                                    height: "50%",
+                                    objectFit: "cover"
+                                }}
                             />
-                            <div className="card-body">
-                                <h5 className="card-title">{product.name}</h5>
-                                <p className="card-text">${product.price}</p>
-                                <Link to={`/product/${product.id}`} className="btn btn-success">Ver detalle</Link>
+
+                            <div className="
+                            card-body
+                            d-flex
+                            flex-column
+                            justify-content-between
+                            ">
+                                <h5 className="
+                                card-title
+                                font-weight-bold
+                                ">{product.name}</h5>
+                                <p className="
+                                card-text
+                                font-weight-bold
+                                fs-4
+                                ">${product.price}</p>
+                                <Link to={`/product/${product.id}`} className="
+                                btn btn-outline-secondary
+                                btn-sm
+                                ">Ver detalle</Link>
                             </div>
                         </div>
                     </div>
