405916b726456da78d2c811807a96668665c40e2
SteveO <orregoe@gmail.com>
2024-01-11 14:09:59 -0300

    Add cart items retrieval from Firebase

--- src/layout/LayOutPublic.jsx
+++ src/layout/LayOutPublic.jsx
@@ -1,8 +1,31 @@
 import { Outlet } from "react-router-dom"
 import NavbarPage from "../components/Navbar/NavbarPage"
 import Footer from "../components/Footer"
+import { useAuth } from "../context/FirebaseAuthContext";
+import { useContext, useEffect } from "react";
+import { CartContext } from "../context/CartContext";
+import { db } from "../services/firebase/config.js";
+import { collection, getDocs, query, where } from "firebase/firestore";
 
 const LayOutPublic = () => {
+    const { currentUser } = useAuth();
+    const { setCartItems } = useContext(CartContext);
+
+    // trae los items del carrito del usuario logueado desde firebase y los guarda en el contexto del carrito para que esten disponibles en toda la app
+    useEffect(() => {
+        const getCartItemsFirebase = async () => {
+            if (currentUser) {
+                const cartItemsCollection = collection(db, 'cartPending');
+                const cartItemsQuery = query(cartItemsCollection, where('userId', '==', currentUser.uid));
+                const cartItemsSnapshot = await getDocs(cartItemsQuery);
+                const cartItemsList = cartItemsSnapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
+                const cartItems = cartItemsList[0]?.cartItems || [];
+                setCartItems(cartItems);
+            }
+        };
+        getCartItemsFirebase();
+    }, [currentUser, setCartItems]);
+
     return (
         <>
             <NavbarPage />
