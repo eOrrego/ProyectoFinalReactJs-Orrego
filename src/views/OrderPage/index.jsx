0a8a1f85a665986c3e1f128546d27aa19f0a1cf4
Esteban Orrego <orregoe@gmail.com>
2024-01-12 23:25:21 -0300

    Update item title to item name in OrderPage

--- src/views/OrderPage/index.jsx
+++ src/views/OrderPage/index.jsx
@@ -62,7 +62,7 @@ const OrderPage = () => {
                         <tbody>
                             {order.items.map((item) => (
                                 <tr key={item.id}>
-                                    <td>{item.title}</td>
+                                    <td>{item.name}</td>
                                     <td>{item.quantity}</td>
                                     <td>${item.price}</td>
                                     <td>${item.price * item.quantity}</td>
