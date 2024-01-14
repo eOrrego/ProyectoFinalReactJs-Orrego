2f98c1ffcddbfe07ab19c103afc11b9562becb3f
Esteban Orrego <orregoe@gmail.com>
2024-01-14 20:13:56 -0300

    Commented out code related to saving cart pending data

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -2,32 +2,32 @@
 import { Button } from "react-bootstrap";
 import { BsPerson } from 'react-icons/Bs';
 import { Navigate, Link } from "react-router-dom";
-import { db } from "../../services/firebase/config.js";
-import { addDoc, collection, Timestamp } from "@firebase/firestore";
+// import { db } from "../../services/firebase/config.js";
+// import { addDoc, collection, Timestamp } from "@firebase/firestore";
 
 const Profile = ({ cartItems, clearCart, currentUser, logout }) => {
 
-    const saveCartPending = async () => {
-        const objCartPending = {
-            cartItems,
-            date: Timestamp.fromDate(new Date()),
-            userId: currentUser.uid,
-        };
+    // const saveCartPending = async () => {
+    //     const objCartPending = {
+    //         cartItems,
+    //         date: Timestamp.fromDate(new Date()),
+    //         userId: currentUser.uid,
+    //     };
 
-        try {
-            const cartPendingRef = collection(db, "cartPending");
-            await addDoc(cartPendingRef, objCartPending);
-        } catch (error) {
-            console.log(error);
-        }
-    };
+    //     try {
+    //         const cartPendingRef = collection(db, "cartPending");
+    //         await addDoc(cartPendingRef, objCartPending);
+    //     } catch (error) {
+    //         console.log(error);
+    //     }
+    // };
 
     const handleLogout = async () => {
+        await logout();
         if (cartItems.length > 0) {
-            await saveCartPending();
+            // await saveCartPending();
             clearCart();
         }
-        logout();
     };
 
     if (!currentUser) {
